fbdbeaa402e6598e487f68730f0367a36e74c491
OZGUR GULER <97877847+ozgurgulerx@users.noreply.github.com>
2023-08-31 10:38:02 +0300

    Update setup.sh

--- cli/setup.sh
+++ cli/setup.sh
@@ -25,9 +25,9 @@ az extension add -n ml -y
 
 ## For backward compatibility - running on old subscription
 # <set_variables>
-GROUP="azureml-examples"
-LOCATION="eastus"
-WORKSPACE="main"
+GROUP="rg-openai"
+LOCATION="westeurope"
+WORKSPACE="2308-workspace-mlops-ozguler"
 # </set_variables>
 
 # If RESOURCE_GROUP_NAME is empty, the az configure is pending.
